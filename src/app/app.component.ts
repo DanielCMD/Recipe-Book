3d5ec1089dfbe443aa90582d456c2d2a65fff533
DanielCMD <abido.daniel@hotmail.com>
2016-08-24 14:10:17 -0300

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header.component'
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-root',
+  templateUrl: 'app.component.html',
+  directives: [HeaderComponent]
+})
+export class AppComponent {
+} 

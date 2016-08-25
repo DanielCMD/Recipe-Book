e0c89c2561f73a43048a0505bc92f53e1761c344
DanielCMD <abido.daniel@hotmail.com>
2016-08-25 17:52:28 -0300

    versao estavel sem lista de ingredientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header.component'
+import { RecipesComponent } from './recipes'
+import { ShoppingListComponent } from './shopping-list'
 
 @Component({
   moduleId: module.id,
   selector: 'rb-root',
   templateUrl: 'app.component.html',
-  directives: [HeaderComponent]
+  directives: [HeaderComponent, RecipesComponent, ShoppingListComponent]
 })
 export class AppComponent {
 } 
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, EventEmitter,Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
+import { RecipeItemComponent } from './recipe-item.component';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-recipe-list',
+  templateUrl: 'recipe-list.component.html',
+  directives: [RecipeItemComponent]
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipe = new Recipe('Dummy', 'Dummy','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn5i-qZnL9evPv4hG9QICjkmhB8s9YXpJM-GwBPHrY-fH943lzb1wX-Q');
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelected(recipe: Recipe) {
+    this.recipeSelected.emit(recipe);
+  }
+
+}
